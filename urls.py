9ffd9575f2d46f7e424d730008d47ed834457009
stoneff <60143841+stoneff@users.noreply.github.com>
2020-02-01 13:28:38 +0000

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from . import views
 
 from sdfg. improts sdfgsdfg
 
 urlpatterns = [
     path('', views.home, name = 'myblog-home'),
+    path('rango', include('rango.urls')),
     path('about/', views.about, name = 'myblog-about'),
 ]
