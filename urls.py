77d2fa714491cc64b3298e285d82d11e02d7b1f4
stoneff <60143841+stoneff@users.noreply.github.com>
2020-02-01 01:36:20 +0000

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from sdfg. improts sdfgsdfg
+
 urlpatterns = [
     path('', views.home, name = 'myblog-home'),
     path('about/', views.about, name = 'myblog-about'),
